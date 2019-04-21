161bed89d4840032ffee27097219f034e849d7d1
tjhillard <tjhillard@gmail.com>
2019-04-21 14:17:49 -0700

    adds interactivity tests

---
+++ __test__/interactivity/interactivity.classes.js
@@ -0,0 +1,104 @@
+module.exports = [
+  // appearance
+  {
+    className: '.appearance-none',
+    expectedCSS: [
+      ['appearance', 'none'],
+    ],
+  },
+  // cursor
+  {
+    className: '.cursor-auto',
+    expectedCSS: [
+      ['cursor', 'auto'],
+    ],
+  },
+  {
+    className: '.cursor-default',
+    expectedCSS: [
+      ['cursor', 'default'],
+    ],
+  },
+  {
+    className: '.cursor-pointer',
+    expectedCSS: [
+      ['cursor', 'pointer'],
+    ],
+  },
+  {
+    className: '.cursor-wait',
+    expectedCSS: [
+      ['cursor', 'wait'],
+    ],
+  },
+  {
+    className: '.cursor-move',
+    expectedCSS: [
+      ['cursor', 'move'],
+    ],
+  },
+  {
+    className: '.cursor-not-allowed',
+    expectedCSS: [
+      ['cursor', 'not-allowed'],
+    ],
+  },
+  // outline
+  {
+    className: '.outline-none',
+    expectedCSS: [
+      ['outline', 'none'],
+    ],
+  },
+  // pointer events
+  {
+    className: '.pointer-events-none',
+    expectedCSS: [
+      ['pointer-events', 'none'],
+    ],
+  },
+  {
+    className: '.pointer-events-auto',
+    expectedCSS: [
+      ['pointer-events', 'auto'],
+    ],
+  },
+  // resize
+  {
+    className: '.resize-none',
+    expectedCSS: [
+      ['resize', 'none'],
+    ],
+  },
+  {
+    className: '.resizable',
+    expectedCSS: [
+      ['resize', 'both'],
+    ],
+  },
+  {
+    className: '.resizable-x',
+    expectedCSS: [
+      ['resize', 'horizontal'],
+    ],
+  },
+  {
+    className: '.resizable-y',
+    expectedCSS: [
+      ['resize', 'vertical'],
+    ],
+  },
+  // user select
+  {
+    className: '.user-select-text',
+    expectedCSS: [
+      ['user-select', 'text'],
+    ],
+  },
+  {
+    className: '.user-select-none',
+    expectedCSS: [
+      ['user-select', 'none'],
+    ],
+  },
+];
