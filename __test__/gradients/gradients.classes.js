4edf55914fb34a2704a1727d786eb0287adacc1d
tjhillard <tjhillard@gmail.com>
2019-04-24 13:53:04 -0700

    adds gradients tests

---
+++ __test__/gradients/gradients.classes.js
@@ -0,0 +1,10 @@
+module.exports = [
+  // gradient background
+  {
+    // TODO: not working for unknown reason
+    className: '.bg-gradient-snazzy',
+    expectedCSS: [
+      ['background-image', ''],
+    ],
+  },
+];
