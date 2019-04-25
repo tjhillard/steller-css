99d85e796329a27e060d7b7e1ace3cbcc28a5d36
tjhillard <tjhillard@gmail.com>
2019-04-24 20:45:38 -0700

    updates box-shadow class syntax

--- __test__/effects/effects.classes.js
+++ __test__/effects/effects.classes.js
@@ -1,19 +1,19 @@
 module.exports = [
   // box shadow
   {
-    className: '.box-shadow-sm',
+    className: '.shadow-sm',
     expectedCSS: [
       ['box-shadow', '0 7px 15px 0 rgba(0, 0, 0, 0.11), 0 1px 8px 0 rgba(0, 0, 0, 0.06)'],
     ],
   },
   {
-    className: '.box-shadow-md',
+    className: '.shadow-md',
     expectedCSS: [
       ['box-shadow', '0 10px 20px 0 rgba(0, 0, 0, 0.22), 0 3px 12px 0 rgba(0, 0, 0, 0.07)'],
     ],
   },
   {
-    className: '.box-shadow-lg',
+    className: '.shadow-lg',
     expectedCSS: [
       ['box-shadow', '0 15px 30px 0 rgba(0, 0, 0, 0.33), 0 5px 15px 0 rgba(0, 0, 0, 0.1)'],
     ],
